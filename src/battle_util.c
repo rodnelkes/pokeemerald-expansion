df9baeb28f53aa9996f11ebad1b5d3392502d2f8
rodnelkes <rodnelkes@gmail.com>
2025-08-09 20:10:06 -0400

    Add level multiplier that scales from level 5 to level 100 at Elite Four

--- src/battle_util.c
+++ src/battle_util.c
@@ -7446,22 +7446,22 @@ u8 GetAttackerObedienceForAction()
     if (FlagGet(FLAG_BADGE08_GET)) // Rain Badge, ignore obedience altogether
         return OBEYS;
 
-    obedienceLevel = 10;
+    obedienceLevel = GetLevelAfterMultiplier(10);
 
     if (FlagGet(FLAG_BADGE01_GET)) // Stone Badge
-        obedienceLevel = 20;
+        obedienceLevel = GetLevelAfterMultiplier(20);
     if (FlagGet(FLAG_BADGE02_GET)) // Knuckle Badge
-        obedienceLevel = 30;
+        obedienceLevel = GetLevelAfterMultiplier(30);
     if (FlagGet(FLAG_BADGE03_GET)) // Dynamo Badge
-        obedienceLevel = 40;
+        obedienceLevel = GetLevelAfterMultiplier(40);
     if (FlagGet(FLAG_BADGE04_GET)) // Heat Badge
-        obedienceLevel = 50;
+        obedienceLevel = GetLevelAfterMultiplier(50);
     if (FlagGet(FLAG_BADGE05_GET)) // Balance Badge
-        obedienceLevel = 60;
+        obedienceLevel = GetLevelAfterMultiplier(60);
     if (FlagGet(FLAG_BADGE06_GET)) // Feather Badge
-        obedienceLevel = 70;
+        obedienceLevel = GetLevelAfterMultiplier(70);
     if (FlagGet(FLAG_BADGE07_GET)) // Mind Badge
-        obedienceLevel = 80;
+        obedienceLevel = GetLevelAfterMultiplier(80);
 
     if (B_OBEDIENCE_MECHANICS >= GEN_8
      && !IsOtherTrainer(gBattleMons[gBattlerAttacker].otId, gBattleMons[gBattlerAttacker].otName))
