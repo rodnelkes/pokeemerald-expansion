5f78c0455b610f6191f1584c6db4dc711c0cb579
rodnelkes <rodnelkes@gmail.com>
2025-08-28 02:32:26 -0400

    Use named randomizer vars/flags for instead

--- include/config/randomizer.h
+++ include/config/randomizer.h
@@ -5,28 +5,28 @@
 // Global control. If FALSE, no randomizer functionality will be enabled.
 // If this is TRUE, that doesn't necessarily mean that a particular part of the randomizer
 // will be enabled.
-#define RANDOMIZER_AVAILABLE                   TRUE
-#define RANDOMIZER_SEED_IS_TRAINER_ID          TRUE
+#define RANDOMIZER_AVAILABLE                          TRUE
+#define RANDOMIZER_SEED_IS_TRAINER_ID                 TRUE
 
 #if RANDOMIZER_AVAILABLE == TRUE
 
 // If TRUE, the trainer ID (including secret ID) will be the randomizer seed.
-#define RZ_TRAINER_ID_IS_SEED       TRUE
+#define RZ_TRAINER_ID_IS_SEED                         TRUE
 
 // If TRUE, dynamically generated randomization tables stored in EWRAM are used.
 // This consumes 6 bytes for each species present.
-#define RANDOMIZER_DYNAMIC_SPECIES    TRUE
+#define RANDOMIZER_DYNAMIC_SPECIES                    TRUE
 
 #if RANDOMIZER_DYNAMIC_SPECIES == TRUE
 
 // If the longest evolutionary chain (excluding babies) is longer than this,
 // the dynamic evolutionary stage randomization table will be generated
 // incorrectly.
-#define RANDOMIZER_MAX_EVO_STAGES   5
+#define RANDOMIZER_MAX_EVO_STAGES                     5
 
 #endif // RANDOMIZER_DYNAMIC_SPECIES
 
-#define RANDOMIZER_MAX_TM           ITEM_TM50
+#define RANDOMIZER_MAX_TM                             ITEM_TM50
 
 // Vars and features
 
@@ -35,64 +35,65 @@
 // If undefined, the feature will be enabled if one of the flags below is set.
 // If defined and set to TRUE, the feature will always be enabled.
 // If defined and set to FALSE, the feature will always be disabled.
-#define FORCE_RANDOMIZE_WILD_MON                  TRUE
-#define FORCE_RANDOMIZE_FIELD_ITEMS               TRUE
-#define FORCE_RANDOMIZE_TRAINER_MON               TRUE
-#define FORCE_RANDOMIZE_FIXED_MON                 TRUE
-#define FORCE_RANDOMIZE_STARTER_AND_GIFT_MON      TRUE
-#define FORCE_RANDOMIZE_EGG_MON                   TRUE
-#define FORCE_RANDOMIZE_ABILITIES                 TRUE
-#define FORCE_RANDOMIZE_TMS_AND_HMS               TRUE
-#define FORCE_RANDOMIZE_MOVE_TUTORS               TRUE
-#define FORCE_RANDOMIZE_BERRIES                   TRUE
+#define FORCE_RANDOMIZE_WILD_MON                      TRUE
+#define FORCE_RANDOMIZE_FIELD_ITEMS                   TRUE
+#define FORCE_RANDOMIZE_TRAINER_MON                   TRUE
+#define FORCE_RANDOMIZE_FIXED_MON                     TRUE
+#define FORCE_RANDOMIZE_STARTER_AND_GIFT_MON          TRUE
+#define FORCE_RANDOMIZE_EGG_MON                       TRUE
+#define FORCE_RANDOMIZE_ABILITIES                     TRUE
+#define FORCE_RANDOMIZE_TMS_AND_HMS                   TRUE
+#define FORCE_RANDOMIZE_MOVE_TUTORS                   TRUE
+#define FORCE_RANDOMIZE_BERRIES                       TRUE
 
 // These flags control whether a particular randomization feature is active.
 // They are ignored and disabled if the flags above are set.
 #ifndef FORCE_RANDOMIZE_WILD_MON
-#define RANDOMIZER_FLAG_WILD_MON                      FLAG_UNUSED_0x020
+#define RANDOMIZER_FLAG_WILD_MON                      FLAG_RANDOMIZER_WILD_MON
 #endif
 
 #ifndef FORCE_RANDOMIZE_FIELD_ITEMS
-#define RANDOMIZER_FLAG_FIELD_ITEMS                   FLAG_UNUSED_0x021
+#define RANDOMIZER_FLAG_FIELD_ITEMS                   FLAG_RANDOMIZER_FIELD_ITEMS
 #endif
 
 #ifndef FORCE_RANDOMIZE_TRAINER_MON
-#define RANDOMIZER_FLAG_TRAINER_MON                   FLAG_UNUSED_0x022
+#define RANDOMIZER_FLAG_TRAINER_MON                   FLAG_RANDOMIZER_TRAINER_MON
 #endif
 
 #ifndef FORCE_RANDOMIZE_FIXED_MON
-#define RANDOMIZER_FLAG_FIXED_MON                     FLAG_UNUSED_0x023
+#define RANDOMIZER_FLAG_FIXED_MON                     FLAG_RANDOMIZER_FIXED_MON
+// VAR_RANDOMIZER_ZIGZAGOON_PERSONALITY_L and VAR_RANDOMIZER_ZIGZAGOON_PERSONALITY_H need to also be set
 #endif
 
 #ifndef FORCE_RANDOMIZE_STARTER_AND_GIFT_MON
-#define RANDOMIZER_FLAG_STARTER_AND_GIFT_MON          FLAG_UNUSED_0x024
+#define RANDOMIZER_FLAG_STARTER_AND_GIFT_MON          FLAG_RANDOMIZER_STARTER_AND_GIFT_MON
 #endif
 
 #ifndef FORCE_RANDOMIZE_EGG_MON
-#define RANDOMIZER_FLAG_EGG_MON                       FLAG_UNUSED_0x025
+#define RANDOMIZER_FLAG_EGG_MON                       FLAG_RANDOMIZER_EGG_MON
 #endif
 
 #ifndef FORCE_RANDOMIZE_ABILITIES
-#define RANDOMIZER_FLAG_ABILITIES                     FLAG_UNUSED_0x026
+#define RANDOMIZER_FLAG_ABILITIES                     FLAG_RANDOMIZER_ABILITIES
 #endif
 
 #ifndef FORCE_RANDOMIZE_TMS_AND_HMS
-#define RANDOMIZER_FLAG_TMS_AND_HMS                   FLAG_UNUSED_0x027
+#define RANDOMIZER_FLAG_TMS_AND_HMS                   FLAG_RANDOMIZER_TMS_AND_HMS
 #endif
 
 #ifndef FORCE_RANDOMIZE_MOVE_TUTORS
-#define RANDOMIZER_FLAG_MOVE_TUTORS                   FLAG_UNUSED_0x028
+#define RANDOMIZER_FLAG_MOVE_TUTORS                   FLAG_RANDOMIZER_MOVE_TUTORS
 #endif
 
 #ifndef FORCE_RANDOMIZE_BERRIES
-#define RANDOMIZER_FLAG_BERRIES                       FLAG_UNUSED_0x029
+#define RANDOMIZER_FLAG_BERRIES                       FLAG_RANDOMIZER_BERRIES
 #endif
 
 #define RANDOMIZER_VAR_SPECIES_MODE                   VAR_RANDOMIZER_SPECIES_MODE
 
 #if RANDOMIZER_SEED_IS_TRAINER_ID == FALSE
-#define RANDOMIZER_VAR_SEED_L                         VAR_UNUSED_0x40FA
-#define RANDOMIZER_VAR_SEED_H                         VAR_UNUSED_0x40FB
+#define RANDOMIZER_VAR_SEED_L                         VAR_RANDOMIZER_SEED_L
+#define RANDOMIZER_VAR_SEED_H                         VAR_RANDOMIZER_SEED_H
 #endif
 
 #endif // RANDOMIZER_AVAILABLE
--- include/constants/vars.h
+++ include/constants/vars.h
@@ -95,7 +95,7 @@
 #define VAR_POKELOT_RND1                                 0x404B
 #define VAR_POKELOT_RND2                                 0x404C
 #define VAR_POKELOT_PRIZE_PLACE                          0x404D
-#define VAR_ITEM_LAST_REPEL_LURE_USED                    0x404E // Previously Unused Var
+#define VAR_RANDOMIZER_SPECIES_MODE                      0x404E // Previously Unused Var
 #define VAR_LOTAD_SIZE_RECORD                            0x404F
 #define VAR_LITTLEROOT_TOWN_STATE                        0x4050
 #define VAR_OLDALE_TOWN_STATE                            0x4051
@@ -148,7 +148,7 @@
 #define VAR_ROUTE133_STATE                               0x4080 // Unused Var
 #define VAR_ROUTE134_STATE                               0x4081 // Unused Var
 #define VAR_LITTLEROOT_HOUSES_STATE_MAY                  0x4082
-#define VAR_RANDOMIZER_SPECIES_MODE                      0x4083 // Previously Unused Var
+#define VAR_RANDOMIZER_ZIGZAGOON_PERSONALITY_L           0x4083 // Previously Unused Var
 #define VAR_BIRCH_LAB_STATE                              0x4084
 #define VAR_PETALBURG_GYM_STATE                          0x4085 // 0-1: Wally tutorial, 2-6: 0-4 badges, 7: Defeated Norman, 8: Rematch Norman
 #define VAR_CONTEST_HALL_STATE                           0x4086
@@ -156,13 +156,13 @@
 #define VAR_CONTEST_TYPE                                 0x4088
 #define VAR_SECRET_BASE_INITIALIZED                      0x4089
 #define VAR_CONTEST_PRIZE_PICKUP                         0x408A
-#define VAR_RANDOMIZER_ZIGZAGOON_PERSONALITY_L           0x408B // Previously Unused Var
+#define VAR_RANDOMIZER_ZIGZAGOON_PERSONALITY_H           0x408B // Previously Unused Var
 #define VAR_LITTLEROOT_HOUSES_STATE_BRENDAN              0x408C
 #define VAR_LITTLEROOT_RIVAL_STATE                       0x408D
 #define VAR_BOARD_BRINEY_BOAT_STATE                      0x408E
 #define VAR_DEVON_CORP_3F_STATE                          0x408F
 #define VAR_BRINEY_HOUSE_STATE                           0x4090
-#define VAR_RANDOMIZER_ZIGZAGOON_PERSONALITY_H           0x4091 // Previously Unused Var
+#define VAR_ITEM_LAST_REPEL_LURE_USED                    0x4091 // Previously Unused Var
 #define VAR_LITTLEROOT_INTRO_STATE                       0x4092
 #define VAR_MAUVILLE_GYM_STATE                           0x4093
 #define VAR_LILYCOVE_MUSEUM_2F_STATE                     0x4094
